321cb8fcc51a657a1fc3a826ee809a32053d165c
basdevelop <hyperorchid@outlook.com>
2025-01-06 18:03:12 +0800

    save

--- ios/oneLock/wallet/CreateWalletView.swift
+++ ios/oneLock/wallet/CreateWalletView.swift
@@ -6,7 +6,7 @@ struct CreateWalletView: View {
         @State private var errorMessage: String? = nil
         @State private var mnemonic: String? = nil
         @EnvironmentObject var appState: AppState // 添加环境对象引用
-        @Environment(\.dismiss) private var dismiss // 用于返回上一页面
+        @Environment(\.dismiss) private var dismiss
         
         var body: some View {
                 if let mnemonicPhrase = mnemonic {
@@ -14,7 +14,7 @@ struct CreateWalletView: View {
                         MnemonicView(mnemonic: mnemonicPhrase, password: password)
                                 .environmentObject(appState)
                 } else {
-                        // 显示创建钱包视图
+                        
                         VStack {
                                 
                                 VStack(spacing: 0) {
@@ -24,8 +24,8 @@ struct CreateWalletView: View {
                                                 Image("password-img")
                                                         .resizable()
                                                         .scaledToFill()
-                                                        .frame(width: UIScreen.main.bounds.width, height: 262)
-                                                        .clipped().ignoresSafeArea(edges: .top)
+                                                        .frame(width: UIScreen.main.bounds.width, height: 215)
+                                                        .clipped()
                                                 
                                                 VStack(alignment: .leading, spacing: 8) {
                                                         // 标题
@@ -37,29 +37,25 @@ struct CreateWalletView: View {
                                                 .padding(.top, 14)
                                                 .padding(.leading, 21)
                                         }
-                                        
-                                        Spacer() // 为了占据剩余的空间
                                 }
                                 
                                 // Section 2 (覆盖部分)
-                                VStack(spacing: 12) {
+                                VStack(spacing: 24) { // 调整间距
                                         // 输入框 1
                                         SecureField("Enter Password", text: $password)
-                                                .padding(.horizontal, 16)
-                                                .frame(maxWidth: .infinity)
-                                                .frame(width: UIScreen.main.bounds.width - 48, height: 50)
+                                                .padding(.horizontal, 16) // 内边距
+                                                .frame(height: 50) // 高度
                                                 .background(Color(red: 243 / 255, green: 243 / 255, blue: 243 / 255)) // 背景颜色
-                                                .cornerRadius(16) // 圆角
+                                                .cornerRadius(24) // 圆角
                                                 .font(.system(size: 16)) // 字体
                                                 .foregroundColor(Color(red: 137 / 255, green: 145 / 255, blue: 155 / 255)) // 字体颜色
                                         
                                         // 输入框 2
                                         SecureField("Confirm Password", text: $confirmPassword)
                                                 .padding(.horizontal, 16)
-                                                .frame(maxWidth: .infinity)
-                                                .frame(width: UIScreen.main.bounds.width - 48, height: 50)
+                                                .frame(height: 50)
                                                 .background(Color(red: 243 / 255, green: 243 / 255, blue: 243 / 255))
-                                                .cornerRadius(16)
+                                                .cornerRadius(24)
                                                 .font(.system(size: 16))
                                                 .foregroundColor(Color(red: 137 / 255, green: 145 / 255, blue: 155 / 255))
                                         
@@ -75,7 +71,7 @@ struct CreateWalletView: View {
                                         }) {
                                                 RoundedRectangle(cornerRadius: 31)
                                                         .fill(Color(red: 15 / 255, green: 211 / 255, blue: 212 / 255))
-                                                        .frame(width: UIScreen.main.bounds.width - 48, height: 50)
+                                                        .frame(height: 50)
                                                         .overlay(
                                                                 Text("Generate Wallet")
                                                                         .font(.system(size: 16, weight: .semibold))
@@ -83,26 +79,25 @@ struct CreateWalletView: View {
                                                         )
                                         }
                                 }
-                                .background(Color.blue.opacity(0.2)) // 临时背景色用于可视化布局
-                                
+                                .background(Color.red.opacity(0.2))
+                                .clipShape(RoundedRectangle(cornerRadius: 10))
                         }
                         .contentShape(Rectangle()) // 确保手势覆盖整个区域
                         .onTapGesture {
                                 UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
-                        }
-                        .navigationBarBackButtonHidden(true) // 隐藏系统返回按钮
-                        .toolbar {
-                                ToolbarItem(placement: .navigationBarLeading) {
-                                        Button(action: {
-                                                dismiss() // 自定义返回逻辑
-                                        }) {
-                                                Image("back_icon") // 使用自定义的图片
-                                                        .resizable()
-                                                        .scaledToFit()
-                                                        .frame(width: 24, height: 24) // 调整图片大小
+                        }.navigationBarBackButtonHidden(true)
+                                .toolbar {
+                                        ToolbarItem(placement: .navigationBarLeading) {
+                                                Button(action: {
+                                                        dismiss()
+                                                }) {
+                                                        Image("back_icon")
+                                                                .resizable()
+                                                                .scaledToFit()
+                                                                .frame(width: 24, height: 24)
+                                                }
                                         }
                                 }
-                        }
                 }
         }
         
