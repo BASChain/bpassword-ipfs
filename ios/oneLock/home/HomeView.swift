d597d6cc11244871e63425bd31242a5cba868ac7
basdevelop <hyperorchid@outlook.com>
2025-01-08 14:53:15 +0800

    home view

--- ios/oneLock/home/HomeView.swift
+++ ios/oneLock/home/HomeView.swift
@@ -1,4 +1,5 @@
 import SwiftUI
+
 struct HomeView: View {
         @State private var accounts: [UUID: Account] = [:]
         @State private var hasLoaded = false // 防止重复加载
@@ -10,31 +11,46 @@ struct HomeView: View {
                                         Text("No accounts found.")
                                                 .foregroundColor(.gray)
                                                 .padding()
-                                } else {
-                                        List(sortedAccounts(), id: \.id) { account in
-                                                NavigationLink(
-                                                        destination: AccountDetailView(
-                                                                account: bindingForAccount(account), // 传递绑定
-                                                                onAccountDeleted: { hasLoaded = false } // 设置 hasLoaded 为 false
-                                                        )
-                                                ) {
-                                                        HStack {
-                                                                Text(account.platform)
-                                                                        .font(.headline)
-                                                                Spacer()
-                                                                Text(account.formattedLastUpdated())
-                                                                        .font(.subheadline)
-                                                                        .foregroundColor(.gray)
+                                } else  {
+                                        ScrollView {
+                                                VStack(spacing: 12) {
+                                                        ForEach(sortedAccounts(), id: \.id) { account in
+                                                                NavigationLink(destination: AccountDetailView(account: bindingForAccount(account))) {
+                                                                        HStack {
+                                                                                VStack(alignment: .leading, spacing: 5) {
+                                                                                        Text(account.platform)
+                                                                                                .font(.custom("SF Pro Text", size: 20).weight(.medium))
+                                                                                                .foregroundColor(Color.black)
+                                                                                }
+                                                                                Spacer()
+                                                                                Text(account.formattedLastUpdated())
+                                                                                        .font(.custom("Helvetica Neue", size: 16))
+                                                                                        .foregroundColor(Color(red: 137/255, green: 145/255, blue: 155/255))
+                                                                                Image("next_icon")
+                                                                                        .resizable()
+                                                                                        .frame(width: 16, height: 16)
+                                                                        }
+                                                                        .frame(height: 52) // 设置条目高度
+                                                                        .padding(8) // 设置元素的内部间距
+                                                                        .background(Color(red: 243/255, green: 249/255, blue: 250/255))
+                                                                        .cornerRadius(13) // 设置圆角背景
+                                                                }
+                                                                .buttonStyle(PlainButtonStyle()) // 确保没有默认样式
                                                         }
                                                 }
+                                                .padding(.horizontal, 16) // 设置列表整体左右间距
                                         }
                                 }
                         }
-                        .navigationTitle("Account Manager")
+                        .navigationTitle("")
                         .toolbar {
+                                ToolbarItem(placement: .principal) {
+                                        Text("Account Manager")
+                                                .font(.custom("SF Pro Text", size: 18).weight(.medium))
+                                }
                                 ToolbarItem(placement: .navigationBarTrailing) {
                                         NavigationLink(destination: AddAccountView(onSave: loadAccounts)) {
-                                                Image(systemName: "plus")
+                                                Image("add_icon")
                                         }
                                 }
                         }
