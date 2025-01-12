e0a324f10700d021a45c0aff60f8e410c3f5b744
basdevelop <hyperorchid@outlook.com>
2025-01-12 19:00:45 +0800

    bug fixed

--- ios/oneLock/authenticator/NewAuthAccountView.swift
+++ ios/oneLock/authenticator/NewAuthAccountView.swift
@@ -23,7 +23,9 @@ struct NewAuthAccountView: View {
                                                 .background(Color(red: 243 / 255, green: 243 / 255, blue: 243 / 255))
                                                 .cornerRadius(32)
                                                 .padding(.horizontal, 20)
+                                                .frame(height: 46) //  
                                 }
+                                .padding(.top, -28)
                                 
                                 // Account Field Group
                                 VStack(alignment: .leading, spacing: 8) {
@@ -37,6 +39,7 @@ struct NewAuthAccountView: View {
                                                 .background(Color(red: 243 / 255, green: 243 / 255, blue: 243 / 255))
                                                 .cornerRadius(32)
                                                 .padding(.horizontal, 20)
+                                                .frame(height: 46) //
                                 }
                                 .padding(.top, 6)
                                 
@@ -52,6 +55,7 @@ struct NewAuthAccountView: View {
                                                 .background(Color(red: 243 / 255, green: 243 / 255, blue: 243 / 255))
                                                 .cornerRadius(32)
                                                 .padding(.horizontal, 20)
+                                                .frame(height: 46) //
                                 }
                                 .padding(.top, 6)
                                 // Save 按钮
@@ -61,36 +65,34 @@ struct NewAuthAccountView: View {
                                         Text("Save")
                                                 .font(.system(size: 19, weight: .bold)) // 调整按钮字体大小和权重
                                                 .frame(maxWidth: .infinity)
-                                                .frame(height: 46) // 增加按钮高度
+                                                .frame(height: 46) //
                                                 .background(Color(red: 15 / 255, green: 211 / 255, blue: 212 / 255))
                                                 .foregroundColor(.white)
                                                 .cornerRadius(32) // 增加按钮的圆角
                                                 .padding(.horizontal, 20)
                                 }
-                                .padding(.top, 12)
-                                .padding(.bottom, geometry.safeAreaInsets.bottom + 24) // 增加底部间距
-                        }.padding(.top, 60)
-                                .navigationBarHidden(true)
-                                .navigationBarBackButtonHidden(true)
-                                .toolbar(content: {
-                                        ToolbarItem(placement: .principal) {
-                                                Text("Add Account")
-                                                        .font(.custom("SFProText-Medium", size: 18))
-                                                        .foregroundColor(Color.black)
-                                        }
-                                        ToolbarItem(placement: .navigationBarLeading) {
-                                                Button(action: {
-                                                        presentationMode.wrappedValue.dismiss()
-                                                }) {
-                                                        Image("back_icon")
-                                                                .resizable()
-                                                                .frame(width: 24, height: 24)
-                                                }
-                                        }
-                                })
-                                .onTapGesture { // 点击非输入区域隐藏键盘
-                                        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+                                .padding(.top, 24)
+                        }
+                        .navigationBarBackButtonHidden(true)
+                        .toolbar(content: {
+                                ToolbarItem(placement: .principal) {
+                                        Text("Add Account")
+                                                .font(.custom("SFProText-Medium", size: 18))
+                                                .foregroundColor(Color.black)
                                 }
+                                ToolbarItem(placement: .navigationBarLeading) {
+                                        Button(action: {
+                                                presentationMode.wrappedValue.dismiss()
+                                        }) {
+                                                Image("back_icon")
+                                                        .resizable()
+                                                        .frame(width: 24, height: 24)
+                                        }
+                                }
+                        })
+                        .onTapGesture { // 点击非输入区域隐藏键盘
+                                UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+                        }
                 }
         }
 }
